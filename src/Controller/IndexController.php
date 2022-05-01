c46e0104b17e216d7b0f2c144cd9cc0d369740f8
Mickel02 <jakub.materla02@gmail.com>
2022-05-01 22:12:46 +0200

    Utwrzenie repozytorium i commit wszystkich plików napisanych do tej pory. Działa logowanie, rejestrowanie nowych użytkowników, a także dodawanie, wyświetlanie, ustawianie jako prywatne oraz usuwanie zdjęć

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Photo;
+use App\Form\UploadPhotoType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(UploadPhotoType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            if ($this->getUser()){
+                /** @var UploadedFile $pictureFileName */
+                $pictureFileName = $form->get('filename')->getData();
+                if ($pictureFileName) {
+                    try {
+                        $originalFileName = pathinfo($pictureFileName->getClientOriginalName(), PATHINFO_FILENAME);
+                        $safeFileName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFileName);
+                        $newFileName = $safeFileName . '_' . uniqid() . '_' . $pictureFileName->guessExtension();
+                        $pictureFileName->move('images/hosting', $newFileName);
+
+                        $entityPhotos = new Photo();
+                        $entityPhotos->setFilename($newFileName);
+                        $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                        $entityPhotos->setUploadedAt(new \DateTimeImmutable());
+                        $entityPhotos->setUser($this->getUser());
+
+                        $em->persist($entityPhotos);
+                        $em->flush();
+
+                        $this->addFlash('success', 'Dodano zdjęcie!');
+                    }
+                    catch (\Exception $exception) {
+                        $this->addFlash('error', 'Wystąpił nieoczekiwany błąd!');
+                    }
+                }
+
+            }
+        }
+
+        return $this->render('index/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
